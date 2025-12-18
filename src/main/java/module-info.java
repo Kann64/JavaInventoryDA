b078924cd5098938e60d1c5bc14a36f0752cf9a4
Kann64 <kannlebest@gmail.com>
2025-12-18 18:26:45 +0100

    v1

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.inventorymanagerda {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    exports model;
-    exports controller;
-    exports gui;
-    opens gui to javafx.fxml;
-}
